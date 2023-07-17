2d4fb6c7969d8380b998f12565d508b0cd469dfd
Jon Toussaint <jondtoussaint@gmail.com>
2023-07-17 23:06:30 +0300

    Initial Commit

---
+++ Pokedoro/PokedoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedoroApp.swift
+//  Pokedoro
+//
+//  Created by Jon Toussaint on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
